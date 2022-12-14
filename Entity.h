5cb463c6b0a1929ee6dca937808a4b42e9da076a
Anna <koyvestoynen@bk.ru>
2022-12-14 19:23:52 +0300

    Добавлено описание класса Entity.h

---
+++ Entity.h
@@ -0,0 +1,28 @@
+#ifndef __Entity__
+#define __Entity__
+
+#include <SFML/Graphics.hpp>
+
+class Entity {
+public:
+	sf::Texture texture;
+	sf::Sprite sprite;
+	sf::FloatRect GetRect() {return sf::FloatRect(x,y,w,h);};
+		x = X; y = Y; w = W; h = H; name = Name; moveTimer = 0;
+		speed = 0; health = 100; dx = 0; dy = 0;
+		life = true; isMove = false;
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setOrigin(w / 2, h / 2);
+	}
+	bool isAlive() { return life; }; 
+protected:
+	
+	float dx, dy, x, y, speed, moveTimer;
+	int w, h, health;
+	bool life, isMove;
+	sf::String name;
+	enum { left, right, up, down, stay, fly } state;
+};
+
+#endif
