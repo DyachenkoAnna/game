f56d905398c4c134d9c4743b16cb92ed3275d961
Polina <polinababenko02@mail.ru>
2022-12-19 00:35:04 +0300

    he check has the bullet reached the target

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,9 +1,23 @@
-//тут будет реализаци€ методов класса пул€
 #include "Bullet.h"
 
 Bullet::Bullet(sf::Image& image, float X, float Y, int W, int H, float rotation, sf::String Name, int DMG) :Entity(image, X, Y, W, H, Name)
 {
 	//
+	damage = DMG;
+
+	if (damage > 20)
+	{
+		sprite.setScale(0.1 * (damage / 10), 0.1 * (damage / 10));
+		//размер пули зависит от damage
+	}
+	else
+	{
+		sprite.setScale(0.2, 0.2);
+	}
+	speed = 2;
+	sprite.setTextureRect(sf::IntRect(0, 160, 49, 80));
+	sprite.setRotation(rotationPer - 180);
+
 	sprite.setOrigin(w / 2, h / 2);
 	rotationPer = rotation;
 	rotationPer -= 90;
@@ -34,10 +48,30 @@ void Bullet::update(float time, sf::String TileMap[HEIGHT_MAP])
 
 }
 
+int Bullet::GetDamage()
+{
+	life = false;//ѕроверка, что пул€ достигла цели
+	return damage;
+}
+
 sf::FloatRect Bullet::GetRect()
 {
 	sf::FloatRect BufRect;
 	BufRect.height = h;
 	BufRect.width = w;
 	return BufRect;
+}
+
+void Bullet::checkCollisionWithMap(float Dx, float Dy, sf::String TileMap[HEIGHT_MAP])
+{
+	for (int i = y / 32; i < (y + h) / 32; i++)
+		for (int j = x / 32; j < (x + w) / 32; j++)
+		{
+			if (TileMap[i][j] == '0' || TileMap[i][j] == 'b')
+			{
+				life = false;
+				// —толкнулись = пул€ врезалась
+			}
+		}
+	return;
 }
--- Bullet.h
+++ Bullet.h
@@ -1,6 +1,4 @@
 #pragma once
-//тут будет описание класса пуль
-#pragma once
 #include  "Entity.h";
 #include  "Constants.h"
 class : public Entity
@@ -8,8 +6,10 @@ class : public Entity
 public:
 	Bullet(sf::Image& image, float X, float Y, int W, int H, float rotation, sf::String Name, int);
 	void update(float time, sf::String TileMap[HEIGHT_MAP]);
+	int GetDamage();
 	sf::FloatRect GetRect();
-
 private:
+	int damage;
 	float rotationPer;// ѕоворот пули
+	void checkCollisionWithMap(float Dx, float Dy, sf::String TileMap[HEIGHT_MAP]);
 };
