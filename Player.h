f7f8b48541295fd0d3e7d763615d37485c2352af
Anna <koyvestoynen@bk.ru>
2022-12-18 14:56:12 +0300

    Обновлен класс Player

--- Player.h
+++ Player.h
@@ -24,6 +24,7 @@ public:
 	int Getscore() { return score; };
 	void Addscore(int SCR) { score += SCR; };
 	int Gethealth() { return health; };
+	int update(float time, sf::String TileMap[HEIGHT_MAP], sf::Event event);// Жизнь объекта, ф-я вызывается в основной программе
 	//?? GetXY() {  }; // возвращение позиции спрайта героя
 	//?? GetgunXY();// возвращение позиции конца пушки
 	//void struck(int damage);
@@ -39,7 +40,7 @@ private:
 	float gunrotation;
 	int score;
 	float struckTimer;//для анимации нанесения урона
-	//void animation();
+	void animation();
 };
 
 #endif
