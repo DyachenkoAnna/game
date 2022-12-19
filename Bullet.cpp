e172780b672893645136cae21574d0da8c2e8c66
Polina <polinababenko02@mail.ru>
2022-12-19 13:23:09 +0300

    adding animations and checking collisions

--- Bullet.cpp
+++ Bullet.cpp
@@ -2,26 +2,27 @@
 
 Bullet::Bullet(sf::Image& image, float X, float Y, int W, int H, float rotation, sf::String Name, int DMG) :Entity(image, X, Y, W, H, Name)
 {
-	//
 	damage = DMG;
-
-	if (damage > 20)
-	{
-		sprite.setScale(0.1 * (damage / 10), 0.1 * (damage / 10));
-		//размер пули зависит от damage
-	}
-	else
-	{
-		sprite.setScale(0.2, 0.2);
-	}
-	speed = 2;
-	sprite.setTextureRect(sf::IntRect(0, 160, 49, 80));
-	sprite.setRotation(rotationPer - 180);
-
+	TTL = 5000;//Время жизни, ограничивает дальность полета пули
 	sprite.setOrigin(w / 2, h / 2);
 	rotationPer = rotation;
 	rotationPer -= 90;
-	speed = 2;
+	if (name == "HeroBullet")
+	{
+		if (damage > 20)
+		{
+			sprite.setScale(0.1 * (damage / 10), 0.1 * (damage / 10));
+			//Размер пули зависит от damage
+		}
+		else
+		{
+			sprite.setScale(0.2, 0.2);
+		}
+		speed = 2;
+		sprite.setTextureRect(sf::IntRect(0, 160, 49, 80));
+		sprite.setRotation(rotationPer - 180);
+	}
+
 
 	dx = cos(rotationPer / 180 * 3.14159265) * 0.1;
 	dy = sin(rotationPer / 180 * 3.14159265) * 0.085;
@@ -34,17 +35,26 @@ void Bullet::update(float time, sf::String TileMap[HEIGHT_MAP])
 {
 	moveTimer += time;
 	x += dx * time * speed;
-
+	checkCollisionWithMap(dx, 0, TileMap);
 	y += dy * time * speed;
-
+	checkCollisionWithMap(0, dy, TileMap);
+	//Скорость регулируется отдельной переменной
+	//Проверка на столкновения
+	TTL -= time;
+	if (TTL <= 0)
+	{
+		life = false;
+	}
 	if (life)
 	{
 		sprite.setPosition(x + w / 2, y + h / 2);
 	}
 	else
 	{
+		animation();
 		return;
 	}
+	animation();
 
 }
 
@@ -57,9 +67,60 @@ int Bullet::GetDamage()
 sf::FloatRect Bullet::GetRect()
 {
 	sf::FloatRect BufRect;
+
+	BufRect.left = x - w * (cos((rotationPer + 90) / 180 * 3.14159265));
+	BufRect.top = y - h * (sin((rotationPer + 90) / 180 * 3.14159265));
+	//Квадрат вписали в пулю
+
 	BufRect.height = h;
 	BufRect.width = w;
 	return BufRect;
+}
+
+void Bullet::animation()
+{
+	if (name == "HeroBullet")
+	{
+		if (moveTimer < 100)
+		{
+			sprite.setTextureRect(sf::IntRect(0, 160, 49, 80));
+		}
+		else if (moveTimer < 200)
+		{
+			sprite.setTextureRect(sf::IntRect(49, 160, 47, 80));
+		}
+		else if (moveTimer < 300)
+		{
+			sprite.setTextureRect(sf::IntRect(95, 160, 48, 80));
+		}
+		else if (moveTimer < 400)
+		{
+			sprite.setTextureRect(sf::IntRect(142, 160, 48, 80));
+		}
+		else if (moveTimer < 500)
+		{
+			sprite.setTextureRect(sf::IntRect(189, 160, 48, 80));
+		}
+		else if (moveTimer < 600)
+		{
+			sprite.setTextureRect(sf::IntRect(237, 160, 48, 80));
+			moveTimer = 0;
+		}
+		else if (moveTimer < 700)
+		{
+			sprite.setTextureRect(sf::IntRect(219, 80, 48, 80));
+			moveTimer = 0;
+		}
+		else if (moveTimer < 800)
+		{
+			sprite.setTextureRect(sf::IntRect(266, 80, 48, 80));
+
+		}
+		else
+		{
+			moveTimer = 0;
+		}
+	}
 }
 
 void Bullet::checkCollisionWithMap(float Dx, float Dy, sf::String TileMap[HEIGHT_MAP])
--- Bullet.h
+++ Bullet.h
@@ -7,9 +7,12 @@ public:
 	Bullet(sf::Image& image, float X, float Y, int W, int H, float rotation, sf::String Name, int);
 	void update(float time, sf::String TileMap[HEIGHT_MAP]);
 	int GetDamage();
+	sf::String GetName() { return name; };
 	sf::FloatRect GetRect();
 private:
 	int damage;
 	float rotationPer;// Поворот пули
+	float TTL;
+	void animation();
 	void checkCollisionWithMap(float Dx, float Dy, sf::String TileMap[HEIGHT_MAP]);
 };
